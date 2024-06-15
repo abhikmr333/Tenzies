24f15af9ce471a1e5e9bf975bcffbfb3d1c40ed8
abhikmr333 <abhi363642@gmail.com>
2024-06-15 16:49:51 +0530

    added nanoid package to node_modules and using it to generate random id's

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Die from "./Die.js";
+//using nanoid to generate random ids to use as key prop
+import { nanoid } from "nanoid";
 
 export default function App() {
     //creating array of objects which describe the 10 dies
@@ -7,7 +9,8 @@ export default function App() {
         const newDice = [];
         for (let i = 0; i < 10; i++) {
             const randomNum = Math.floor(Math.random() * 6) + 1;
-            newDice.push({ value: randomNum, isHeld: false });
+            //using nanoid to generate ids which will be later used as key props of die component
+            newDice.push({ value: randomNum, isHeld: false, id: nanoid() });
         }
         return newDice;
     }
@@ -16,12 +19,12 @@ export default function App() {
 
     //map over the state variable dice to create 10 Die components
     const dieElements = dice.map((die, index) => {
-        return <Die key={index} value={die.value} />;
+        return <Die key={die.id} value={die.value} />;
     });
 
     //update the state with new random values of dice when roll is clicked
     function rollDice() {
-        //only roll those dice which are not held
+        //updating statue value and only roll those dice which are not held
         setDice((prevValue) => {
             return prevValue.map((die) => {
                 //held then simply no change else change value property to random dice number
