30f064e3b660cec9719348ae7c0f75a625495d2e
abhikmr333 <abhi363642@gmail.com>
2024-06-16 12:36:42 +0530

    added the functionality to hold die

--- src/components/App.js
+++ src/components/App.js
@@ -17,16 +17,38 @@ export default function App() {
     //set array of 10 random dice numbers as lazy state initialization
     const [dice, setDice] = useState(() => allNewDice());
 
+    //holdDie function which will change the value of isHeld from true to false
+    function holdDie(dieId, isHeld) {
+        //toggling the isHeld to true after searching the die using id if it is not held
+        if (!isHeld) {
+            setDice((prevDice) => {
+                return prevDice.map((prevDie) => {
+                    return dieId === prevDie.id
+                        ? { ...prevDie, isHeld: true }
+                        : { ...prevDie };
+                });
+            });
+        }
+    }
+
     //map over the state variable dice to create 10 Die components
-    const dieElements = dice.map((die, index) => {
-        return <Die key={die.id} value={die.value} />;
+    const dieElements = dice.map((die) => {
+        return (
+            <Die
+                isHeld={die.isHeld}
+                key={die.id}
+                value={die.value}
+                //the id of each comoponent is remembered due to closures
+                holdDie={() => holdDie(die.id, die.isHeld)}
+            />
+        );
     });
 
     //update the state with new random values of dice when roll is clicked
     function rollDice() {
-        //updating statue value and only roll those dice which are not held
-        setDice((prevValue) => {
-            return prevValue.map((die) => {
+        //updating state value and only roll those dice which are not held
+        setDice((prevDice) => {
+            return prevDice.map((die) => {
                 //held then simply no change else change value property to random dice number
                 if (die.isHeld) {
                     return { ...die };
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function Die(props) {
     return (
-        <div className="die">
+        //if the die is held apply green bg to it
+        <div
+            onClick={props.holdDie}
+            className={props.isHeld ? "die held" : "die"}
+        >
             <h3>{props.value}</h3>
         </div>
     );
